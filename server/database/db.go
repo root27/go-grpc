51bc4fcef33cf8c46c0a10517a603710123d7b58
root27 <oguzhaneee@gmail.com>
2024-01-30 14:04:47 +0300

    go version issues fix

--- server/database/db.go
+++ server/database/db.go
@@ -2,38 +2,33 @@ package database
 
 import (
 	"context"
+	"log"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type Client struct {
-	*mongo.Client
-}
+var Userdb *mongo.Collection
+var db *mongo.Client
 
-func Connect() (*mongo.Client, error) {
+func Connect() (*mongo.Collection, error) {
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 
-	opt := options.Client().ApplyURI("mongodb://localhost:27017")
-
-	client, err := mongo.Connect(context.Background(), opt)
+	db, err := mongo.Connect(context.Background(), clientOptions)
 
 	if err != nil {
+		log.Fatal(err)
 		return nil, err
-
 	}
 
-	return client, nil
+	err = db.Ping(context.Background(), nil)
 
-}
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
 
-func (c *Client) Database(name string) *mongo.Database {
+	Userdb = db.Database("go-grpc").Collection("users")
 
-	return c.Database(name)
-
-}
-
-func (c *Client) Collection(db, name string) *mongo.Collection {
-
-	return c.Database(db).Collection(name)
-
+	return Userdb, nil
 }
