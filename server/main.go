4c79c8ce2a260cea56c452cda0d471ee3038c56a
root27 <oguzhaneee@gmail.com>
2024-01-30 14:12:05 +0300

    added create user service mongo

--- server/main.go
+++ server/main.go
@@ -6,11 +6,15 @@ import (
 	"net"
 
 	"github.com/root27/go-grpc/pb"
+	"github.com/root27/go-grpc/server/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"google.golang.org/grpc"
 )
 
 type server struct {
 	pb.UnimplementedHelloServiceServer
+	pb.UnimplementedUserServiceServer
 }
 
 func main() {
@@ -23,6 +27,7 @@ func main() {
 	s := grpc.NewServer()
 
 	pb.RegisterHelloServiceServer(s, &server{})
+	pb.RegisterUserServiceServer(s, &server{})
 
 	if err := s.Serve(conn); err != nil {
 		log.Fatalf("failed to serve: %v", err)
@@ -32,4 +37,22 @@ func main() {
 
 func (s *server) Hello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloResponse, error) {
 	return &pb.HelloResponse{Message: "Hello " + in.Name}, nil
+}
+
+func (s *server) Create(ctx context.Context, in *pb.CreateRequest) (*pb.CreateResponse, error) {
+	db, err := database.Connect()
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	res, err := db.InsertOne(context.Background(), bson.M{"name": in.Name, "email": in.Email})
+
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return &pb.CreateResponse{Id: res.InsertedID.(primitive.ObjectID).Hex(), Name: in.Name, Email: in.Email}, nil
 }
